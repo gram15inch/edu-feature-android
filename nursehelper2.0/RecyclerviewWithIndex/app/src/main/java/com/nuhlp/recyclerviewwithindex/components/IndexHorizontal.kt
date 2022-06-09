0821ca9635a80a1711dc0d5cb74554cd070b2afb
gram15inch <leapthestairs@gmail.com>
2022-06-09 09:59:41 +0900

    6/9

--- nursehelper2.0/RecyclerviewWithIndex/app/src/main/java/com/nuhlp/recyclerviewwithindex/components/IndexHorizontal.kt
+++ nursehelper2.0/RecyclerviewWithIndex/app/src/main/java/com/nuhlp/recyclerviewwithindex/components/IndexHorizontal.kt
@@ -10,16 +10,21 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.view.children
 import com.nuhlp.recyclerviewwithindex.R
+import com.nuhlp.recyclerviewwithindex.databinding.IndexHorizontalBinding
 
 
 class IndexHorizontal(context: Context, attrs: AttributeSet) :LinearLayout(context,attrs) {
 
     private lateinit var elements : List<TextView>
     private var indexString : String
-    val v : View
+    var binding : IndexHorizontalBinding
+   // val v : View
     init {
-        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        v= inflater.inflate(R.layout.index_horizontal, this)
+        //val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        //v= inflater.inflate(R.layout.index_horizontal, this)
+       binding = IndexHorizontalBinding.inflate(LayoutInflater.from(context), this,true)
+       setElements("1 2 3 4 5 6 7 8 9 10")
+
         context.theme.obtainStyledAttributes(
             attrs,
             R.styleable.index_horizontal,
@@ -36,6 +41,16 @@ class IndexHorizontal(context: Context, attrs: AttributeSet) :LinearLayout(conte
 
     private fun setElements(str: String) {
         // todo 커스텀 뷰에 글자넣기
+     val a =  str.split(" ")
+        val clist=  binding.root.children
+        val ai= a.iterator()
+        for (c in clist){
+            if(ai.hasNext()){
+                (c as TextView).text= ai.next()
+            }else
+                (c as TextView).text="*"
+        }
+
     /*    val v = v as ViewGroup
             elements = v.children.toList() as List<TextView>
         for(e in v.children.toList())
