8ab4ac257f075012f04dbcce4e8a64e2361a1063
gram15inch <leapthestairs@gmail.com>
2022-07-29 11:31:25 +0900

    7/29

--- mapApi/googleMap/app/src/main/java/com/nuhlp/googlemapapi/util/PermissionUtil.kt
+++ mapApi/googleMap/app/src/main/java/com/nuhlp/googlemapapi/util/PermissionUtil.kt
@@ -6,6 +6,16 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
+
+/* todo 문서 학습
+* 1. map gps 요청 -> 권한 필요
+* 2. 권한 요청 -> ActivityResultLauncher 필요
+* 3. ARL 요청 -> -> ActivityResult 필요
+* 4. AR 요청 -> Contracts, Callback 필요
+*   4-1. Contracts 학습 ->  Intent 생성, 결과코드 해독 등
+*   4-2. Callback 학습 -> 결과처리 :: 호출 액티비티가 재시작 되서 작업순서가 사라져도 결과를 처리할 수 있게 결과처리함수를 직접주입하기 위해 사용
+* 5.
+* */
 abstract class PermissionUtil(ca:AppCompatActivity,permission: String)  {
     init {
         val checkPermission = ContextCompat.checkSelfPermission(ca, permission)
@@ -16,7 +26,7 @@ abstract class PermissionUtil(ca:AppCompatActivity,permission: String)  {
              else
                 rejectPermission()
         }
-
+        //todo 여기부터 시작
         when {
              checkPermission == PackageManager.PERMISSION_GRANTED -> {
                 grantPermission()
